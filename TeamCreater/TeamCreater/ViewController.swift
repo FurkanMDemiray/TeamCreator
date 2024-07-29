1e90f1aac6fa0f01a6f4aa1609827dbffcd27720
FurkanMDemiray <furkanmdemiray@gmail.com>
2024-07-29 10:22:07 +0300

    inital

---
+++ TeamCreater/TeamCreater/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TeamCreater
+//
+//  Created by Melik Demiray on 29.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
