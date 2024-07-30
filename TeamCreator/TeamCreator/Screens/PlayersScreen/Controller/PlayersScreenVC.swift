fd141f3e45d811b09e98f402d490970b7614f4f4
Agah Berkin Güler <yedekguler@gmail.com>
2024-07-30 10:55:32 +0300

    PlayerScreen VM and VC added

---
+++ TeamCreator/TeamCreator/Screens/PlayersScreen/Controller/PlayersScreenVC.swift
@@ -0,0 +1,22 @@
+//
+//  PlayersScreenVC.swift
+//  TeamCreator
+//
+//  Created by Agah Berkin Güler on 30.07.2024.
+//
+
+import UIKit
+
+protocol PlayerScreenVCProtocol: AnyObject {
+    
+}
+
+final class PlayersScreenVC: UIViewController {
+    
+    private let viewModel = PlayerScreenVM()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
