81d9a672c55c86339339ad22750ffe7cabf77559
FurkanMDemiray <furkanmdemiray@gmail.com>
2024-07-30 10:14:33 +0300

    added volleyball, soccer image views

--- TeamCreator/TeamCreator/Screens/HomeScreen/HomeViewController.swift
+++ TeamCreator/TeamCreator/Screens/HomeScreen/HomeViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 final class HomeViewController: UIViewController {
 
+    @IBOutlet weak var soccerImageView: UIImageView!
+    @IBOutlet weak var volleyballImageView: UIImageView!
+
     var viewModel: HomeViewModelProtocol! {
         didSet {
             viewModel.delegate = self
@@ -17,7 +20,33 @@ final class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureImages()
+    }
+
+    private func configureImages() {
+        soccerImageView.isUserInteractionEnabled = true
+        let gestureRecognizerSoccer = UITapGestureRecognizer(target: self, action: #selector(soccerButtonTapped))
+        soccerImageView.addGestureRecognizer(gestureRecognizerSoccer)
+        soccerImageView.layer.cornerRadius = 10
+        soccerImageView.layer.shadowColor = UIColor.black.cgColor
+        soccerImageView.layer.shadowOffset = CGSize(width: 5, height: 5)
+        soccerImageView.layer.shadowRadius = 5
+
+        let gestureRecognizerVolleyball = UITapGestureRecognizer(target: self, action: #selector(volleyballButtonTapped))
+        volleyballImageView.addGestureRecognizer(gestureRecognizerVolleyball)
+        volleyballImageView.isUserInteractionEnabled = true
+        volleyballImageView.layer.cornerRadius = 10
+        volleyballImageView.layer.shadowColor = UIColor.black.cgColor
+        volleyballImageView.layer.shadowOffset = CGSize(width: 5, height: 5)
+        volleyballImageView.layer.shadowRadius = 5
+    }
+
+    @objc func soccerButtonTapped() {
+        //viewModel.soccerButtonTapped()
+    }
+
+    @objc func volleyballButtonTapped() {
+        //viewModel.volleybolButtonTapped()
     }
 
 
