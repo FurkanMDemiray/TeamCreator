527c1236086826009e593e4b2fb4e346edf687e5
FurkanMDemiray <furkanmdemiray@gmail.com>
2024-08-01 16:48:20 +0300

    added soccer team players

--- TeamCreator/TeamCreator/Screens/MatchDetailScreen/MatchDetailTeamScreen/MatchDetailTeamViewController.swift
+++ TeamCreator/TeamCreator/Screens/MatchDetailScreen/MatchDetailTeamScreen/MatchDetailTeamViewController.swift
@@ -9,6 +9,30 @@ import UIKit
 
 class MatchDetailTeamViewController: UIViewController {
 
+    @IBOutlet private weak var gkImage: UIImageView!
+    @IBOutlet private weak var gkLabel: UILabel!
+    @IBOutlet private weak var rbImage: UIImageView!
+    @IBOutlet private weak var rbLabel: UILabel!
+    @IBOutlet private weak var cbImage: UIImageView!
+    @IBOutlet private weak var cbLabel: UILabel!
+    @IBOutlet private weak var secondCBImage: UIImageView!
+    @IBOutlet private weak var secondCBLabel: UILabel!
+    @IBOutlet private weak var lbImage: UIImageView!
+    @IBOutlet private weak var lbLabel: UILabel!
+    @IBOutlet private weak var rwImage: UIImageView!
+    @IBOutlet private weak var rwLabel: UILabel!
+    @IBOutlet private weak var cmImage: UIImageView!
+    @IBOutlet private weak var cmLabel: UILabel!
+    @IBOutlet private weak var secondCMImage: UIImageView!
+    @IBOutlet private weak var secondCMLabel: UILabel!
+    @IBOutlet private weak var lwImage: UIImageView!
+    @IBOutlet private weak var lwLabel: UILabel!
+    @IBOutlet private weak var cfImage: UIImageView!
+    @IBOutlet private weak var cfLabel: UILabel!
+    @IBOutlet private weak var secondCFImage: UIImageView!
+    @IBOutlet private weak var secondCFLabel: UILabel!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
