7a44d4ca7a8a0f4971636fa0432a6ec9c3d59845
Agah Berkin Güler <yedekguler@gmail.com>
2024-08-01 09:44:26 +0300

    add player screen file created

---
+++ TeamCreator/TeamCreator/Screens/AddPlayerScreen/Controller/AddPlayerScreenVC.swift
@@ -0,0 +1,17 @@
+//
+//  AddPlayerScreenVC.swift
+//  TeamCreator
+//
+//  Created by Agah Berkin Güler on 1.08.2024.
+//
+
+import UIKit
+
+final class AddPlayerScreenVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ TeamCreator/TeamCreator/Screens/AddPlayerScreen/ViewModel/AddPlayerScreenVM.swift
@@ -0,0 +1,13 @@
+//
+//  AddPlayerScreenVM.swift
+//  TeamCreator
+//
+//  Created by Agah Berkin Güler on 1.08.2024.
+//
+
+import Foundation
+
+
+final class AddPlayerScreenVM {
+    
+}
